1aacb0e9a98e11945d19fc47b9a3958fe3f8ec2e
Mocksidul Hassan <94691252+Mocksidul-Hassan@users.noreply.github.com>
2026-07-31 19:18:54 +0900

    Update homework13.py

--- lec13/homework13.py
+++ lec13/homework13.py
@@ -16,7 +16,24 @@ def lpc(speech, frame_length, frame_skip, order):
     excitation (nframes,frame_length) - linear prediction excitation frames
       (only the last frame_skip samples in each frame need to be valid)
     '''
-    raise RuntimeError("You need to write this part!")
+    nframes = int((len(speech) - frame_length) / frame_skip)
+    A = np.zeros((nframes, order + 1))
+    excitation = np.zeros((nframes, frame_length))
+    
+    for i in range(nframes):
+        start = i * frame_skip
+        frame = speech[start : start + frame_length]
+        
+        # LPC coefficients (a[0] = 1)
+        a = librosa.lpc(frame, order=order)
+        A[i, :] = a
+        
+        # Residual (excitation)
+        # residual = filter(a, [1], frame)
+        residual = np.convolve(frame, a, mode='full')[:frame_length]
+        excitation[i, :] = residual
+    
+    return A, excitation
 
 def synthesize(e, A, frame_skip):
     '''
@@ -30,7 +47,28 @@ def synthesize(e, A, frame_skip):
     @returns:
     synthesis (duration) - synthetic speech waveform
     '''
-    raise RuntimeError("You need to write this part!")
+    nframes = A.shape[0]
+    duration = nframes * frame_skip
+    synthesis = np.zeros(duration)
+    
+    # All-pole filter: y[n] = e[n] - a1*y[n-1] - a2*y[n-2] - ...
+    order = A.shape[1] - 1
+    y_hist = np.zeros(order)
+    
+    for i in range(nframes):
+        a = A[i, :]
+        for n in range(frame_skip):
+            idx = i * frame_skip + n
+            if idx >= len(e):
+                break
+            # y[n] = e[n] - sum(a[1:] * y_hist)
+            val = e[idx] - np.dot(a[1:], y_hist)
+            synthesis[idx] = val
+            # update history
+            y_hist = np.roll(y_hist, 1)
+            y_hist[0] = val
+    
+    return synthesis
 
 def robot_voice(excitation, T0, frame_skip):
     '''
@@ -45,5 +83,22 @@ def robot_voice(excitation, T0, frame_skip):
     gain (nframes) - gain for each frame
     e_robot (nframes*frame_skip) - excitation for the robot voice
     '''
-    raise RuntimeError("You need to write this part!")
-
+    nframes = excitation.shape[0]
+    frame_length = excitation.shape[1]
+    
+    # Gain = RMS of the last frame_skip samples of each excitation frame
+    gain = np.zeros(nframes)
+    for i in range(nframes):
+        residual = excitation[i, frame_length - frame_skip : frame_length]
+        gain[i] = np.sqrt(np.mean(residual ** 2))
+    
+    # Robot excitation: impulses every T0 samples, scaled by gain
+    e_robot = np.zeros(nframes * frame_skip)
+    for i in range(nframes):
+        start = i * frame_skip
+        # place impulses in this frame region
+        for n in range(0, frame_skip, T0):
+            if start + n < len(e_robot):
+                e_robot[start + n] = -gain[i]
+    
+    return gain, e_robot
